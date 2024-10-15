b18dc6f43249cb54346e7f724cfaa12983f13271
Gilajoanna <gilajoannajohansson@gmail.com>
2024-10-15 20:57:41 +0200

    header, nav and profile image

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,32 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import Header from "@/components/header";
+import "./globals.css";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Gila | Personal portfolio',
-  description: 'Gila is a frontend- and mobile-developer with 2 years of experience.',
-}
+  title: "Gila | Personal portfolio",
+  description:
+    "Gila is a frontend- and mobile-developer with 2 years of experience.",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className}
-      bg-amber-50 text-stone-950 relative`}>
+      <body
+        className={`${inter.className}
+      bg-amber-50 text-stone-950 relative pt-28 sm:pt-36`}
+      >
         <div className="bg-[#fed7aa] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
         <div className="bg-[#ffe4e6] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
 
-          {children}
+        <Header />
+        {children}
       </body>
     </html>
-  )
+  );
 }
