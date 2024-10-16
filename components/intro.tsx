0747c3dd9c87c726678d77fc4589215b92f20149
Gilajoanna <gilajoannajohansson@gmail.com>
2024-10-16 13:52:41 +0200

    prettier format

--- components/intro.tsx
+++ components/intro.tsx
@@ -9,13 +9,19 @@ export default function Intro() {
     <section>
       <div className="flex items-center justify-center">
         <div className="relative">
-        <motion.div
+          <motion.div
             initial={{ opacity: 0, scale: 0 }}
             animate={{ opacity: 1, scale: 1 }}
-        >
-            <Image src="/images/image.png" alt="Gila" width={200} height={200} priority={true} 
-            className="h-24 w-24 rounded-full object-cover border-[0.35rem] border-white shadow-xl"/>
-        </motion.div>
+          >
+            <Image
+              src="/images/image.png"
+              alt="Gila"
+              width={200}
+              height={200}
+              priority={true}
+              className="h-24 w-24 rounded-full object-cover border-[0.35rem] border-white shadow-xl"
+            />
+          </motion.div>
         </div>
       </div>
     </section>
