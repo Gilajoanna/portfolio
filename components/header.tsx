4edcaeff265e1bebbd9419aa36d2014f784b93f7
Gilajoanna <gilajoannajohansson@gmail.com>
2024-10-23 17:52:11 +0200

    creating context ActiveSectionContext and using it in Header for keeping track of and setting state

--- components/header.tsx
+++ components/header.tsx
@@ -1,13 +1,15 @@
 "use client";
 
-import React, { useState } from "react";
+import React from "react";
 import { motion } from "framer-motion";
 import { links } from "@/lib/data";
 import Link from "next/link";
 import clsx from "clsx";
+import { useActiveSectionContext } from "@/context/active-section-context";
 
 export default function Header() {
-  const [activeSection, setActiveSection] = useState("Home");
+  // The custom hook that let us access the activeSection state, the setActiveSection function and make sure it is not null.
+  const { activeSection, setActiveSection } = useActiveSectionContext();
 
   return (
     <header className="z-[999] relative">
---
+++ context/active-section-context.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import React, { useState, createContext } from "react";
+import { links } from "@/lib/data";
+
+type SectionName = (typeof links)[number]["name"];
+
+type ActiveSectionContextProviderProps = {
+  children: React.ReactNode;
+};
+
+type ActiveSectionContextType = {
+  activeSection: SectionName;
+  setActiveSection: React.Dispatch<React.SetStateAction<SectionName>>;
+};
+
+// To be able to use the activeSection state in other components, we need to create a context
+// We wrap everything that needs access to the value with the context provider
+export const ActiveSectionContext =
+  createContext<ActiveSectionContextType | null>(null);
+
+// This component keeps track of the state of the active section
+export default function ActiveSectionContextProvider({
+  children,
+}: ActiveSectionContextProviderProps) {
+  const [activeSection, setActiveSection] = useState<SectionName>("Home");
+
+  return (
+    <ActiveSectionContext.Provider
+      value={{
+        activeSection,
+        setActiveSection,
+      }}
+    >
+      {children}
+    </ActiveSectionContext.Provider>
+  );
+}
+
+// We use a custom hook to handle the null value of the context. We use this hook in components like Header.
+export function useActiveSectionContext() {
+  const context = React.useContext(ActiveSectionContext);
+  if (context == null) {
+    throw new Error(
+      "useActiveSectionContext must be used within a ActiveSectionContextProvider",
+    );
+  }
+  return context;
+}
